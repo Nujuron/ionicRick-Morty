78abff0e1a793269af17945161341b1c65487d06
Nujuron <adri.brito@outlook.es>
2021-03-12 11:34:00 +0000

    userList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'users-list',
+    loadChildren: () => import('./users-list/users-list.module').then( m => m.UsersListPageModule)
+  },
 ];
 
 @NgModule({
